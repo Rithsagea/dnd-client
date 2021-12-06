9013431b9fbe8730d287ac7bec454bfa4a4b2f81
Rithsagea <rithsagea@gmail.com>
2021-12-05 21:00:57 -0500

    rewrote everything. now it's sensible

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CharacterSheet from "./CharacterSheet/CharacterSheet";
-import "./style.css";
+import "./App.css";
 
 function App() {
   return (
--- src/CharacterSheet/CharacterSheet.jsx
+++ src/CharacterSheet/CharacterSheet.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-
-import CharacterHeader from "./CharacterHeader";
-import CharacterBody from "./CharacterBody";
+import Header from "./Header";
 
 const CharacterSheet = () => {
   return (
-    <div className="character-sheet">
-      <CharacterHeader />
-      <CharacterBody />
+    <div>
+      <Header />
     </div>
   );
 };
